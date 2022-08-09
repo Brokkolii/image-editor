e04ac32cc40e04c79673cae0c9e60637bf51c49b
Patrick Girnghuber <p.girnghuber@yahoo.de>
2022-08-09 19:52:00 +0200

    implemented webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   mode: "production",
   output: {
     filename: "image-editor.js",
+    library: {
+      name: "imageEditor",
+      type: "umd",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
