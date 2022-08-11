f0f91b2d01f0d1bbeb354030f07cdb7bfc1467e1
Patrick Girnghuber <p.girnghuber@yahoo.de>
2022-08-11 18:17:27 +0200

    converted image-editor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./css/main.scss";
 import ImageEditor from "./js/image-editor";
 
-export function init(el: HTMLElement, imgSrc: String) {
+export function init(el: HTMLElement, imgSrc: string) {
   return new ImageEditor(el, imgSrc);
 }
