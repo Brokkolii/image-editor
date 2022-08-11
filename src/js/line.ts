9118ea22883e33b43ef4ce9fc4ff9e3e9d18aca4
Patrick Girnghuber <p.girnghuber@yahoo.de>
2022-08-11 17:34:58 +0200

    converted line class

---
+++ src/js/line.ts
@@ -0,0 +1,9 @@
+export default class Line {
+  constructor(public color: string, public width: number, public path: { x: number; y: number }[]) {}
+  addPoint(x: number, y: number) {
+    this.path.push({
+      x: x,
+      y: y,
+    });
+  }
+}
